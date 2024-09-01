305f82300da996c711e909097b8431ac5ab0e666
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2024-08-31 23:21:03 -0400

    Full functionality

--- ingest.sh
+++ ingest.sh
@@ -1,9 +1,18 @@
 #! /usr/local/bin/bash
 
-./ingest_requirement_blocks.py -p
+# Ingest new blocks
+./ingest_requirement_blocks.py -p >> "$$"
+
+# Parse Unparsed aNd Timeouts
+now=$(date "+%Y-%m-%d %H:%M")
+echo "<h1>PUNT Report $now</h1><pre>" > "$$"
 
 # Parse unparsed active blocks with short, then long, timelimits
-echo -e "\nUnparsed:"
-"$dgws"/parse_active.py -u
-echo -e "\nTimeouts:"
-"$dgws"/parse_active.py -tl 3600
+echo -e "\nUnparsed:" >> "$$"
+"$dgws"/parse_active.py -u >> "$$"
+echo -e "\nTimeouts:" >> "$$"
+"$dgws"/parse_active.py -tl 3600 >> "$$"
+echo "</pre>" >> "$$"
+# Email PUNT report
+sendemail -s "PUNT from $(hostname)" -h "$$" christopher.vickery@qc.cuny.edu
+rm -f "$$"
